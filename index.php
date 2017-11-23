e98b2281b1eedea7f336d02b01bf6339bce13d5b
Jaymeson Heller <helljm@kingschester.co.uk>
2017-11-23 12:23:51 +0000

    This is my first change to this page im gonna deploy it to huroku

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title><?php echo 'hello'?></title>
+    </head>
+<body>
+<p>uh</p>
+<ul>
+    <li>I've completed some of codeacademy</li>
+    <li>Helped with computing club</li>
+    <li>Learned python</li>
+    <li>A* in GCSE</li>
+    <li>I've completed some of codeacademy</li>
+
+
+
+</ul>
+
+<style>
+body {background-color: powderblue;}
+h1 {
+    color: blue;
+    font-family: courier;
+    font-size: 300%;
+}
+p  {
+    color: red;
+    font-family: courier;
+    font-size: 160%;
+}
+</style>
+</body>
+</html>
